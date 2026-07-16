15a0bf411686212ef17e7fbf820335f052baa9a6
valua7777-maker <valua7777@gmail.com>
2026-07-16 17:25:43 +0900

    virtual-cam.h

--- src/virtual-source/virtual-cam.h
+++ src/virtual-source/virtual-cam.h
@@ -13,6 +13,10 @@ EXTERN_C const GUID CLSID_OBS_VirtualV;
 EXTERN_C const GUID CLSID_OBS_VirtualV2;
 EXTERN_C const GUID CLSID_OBS_VirtualV3;
 EXTERN_C const GUID CLSID_OBS_VirtualV4;
+EXTERN_C const GUID CLSID_OBS_VirtualV5; // ★ added
+EXTERN_C const GUID CLSID_OBS_VirtualV6; // ★ added
+EXTERN_C const GUID CLSID_OBS_VirtualV7; // ★ added
+EXTERN_C const GUID CLSID_OBS_VirtualV8; // ★ added
 
 class CVCamStream;
 
@@ -32,6 +36,10 @@ extern CUnknown *WINAPI CreateInstance(LPUNKNOWN lpunk, HRESULT *phr);
 extern CUnknown *WINAPI CreateInstance2(LPUNKNOWN lpunk, HRESULT *phr);
 extern CUnknown *WINAPI CreateInstance3(LPUNKNOWN lpunk, HRESULT *phr);
 extern CUnknown *WINAPI CreateInstance4(LPUNKNOWN lpunk, HRESULT *phr);
+extern CUnknown *WINAPI CreateInstance5(LPUNKNOWN lpunk, HRESULT *phr); // ★ added
+extern CUnknown *WINAPI CreateInstance6(LPUNKNOWN lpunk, HRESULT *phr); // ★ added
+extern CUnknown *WINAPI CreateInstance7(LPUNKNOWN lpunk, HRESULT *phr); // ★ added
+extern CUnknown *WINAPI CreateInstance8(LPUNKNOWN lpunk, HRESULT *phr); // ★ added
 
 class CVCam : public CSource {
 public:
